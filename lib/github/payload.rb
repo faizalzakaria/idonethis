6658f2e34e2eae8713e88e7b35bc7b5b14906212
Faizal Zakaria <phaibusiness@gmail.com>
2014-10-28 01:02:24 +0800

    Generate all the commits details

--- lib/github/payload.rb
+++ lib/github/payload.rb
@@ -6,6 +6,13 @@ module Github
       @raw_payload = raw_payload
     end
 
+    def generate_details
+      params = { details: commit_details }
+      task = pusher.tasks.create(params) if pusher
+    end
+
+    private
+
     def data
       # TODO: error handling on not JSON or not HASH
       @data ||= Hashie::Mash.new(JSON.parse(@raw_payload))
@@ -18,15 +25,28 @@ module Github
 
     def pusher
       return nil unless data.pusher
-      @pusher ||= User.find_by_username(data.pusher.name)
+      @pusher ||= User.find_by_username(data.pusher.name) || User.find_by_email(data.pusher.email)
     end
 
-    def generate_details
-      # 3 commits in repo on branch
-      commits_count = data.commits.try(:size) || 0
-      params = { details: "#{pluralize(commits_count, 'commit')} in <a href=#{data.repository.html_url}>#{data.repository.name}</a> on #{branch}" }
-      puts "params: #{params}"
-      task = pusher.tasks.create(params) if pusher
+    def create_link(url, text)
+      "<a href=#{url}>#{text}</a>"
+    end
+
+    def branch_url
+      "#{data.repository.html_url}/tree/#{data.ref}"
+    end
+
+    def commits
+      @commits = data.commits || []
+    end
+
+    def commit_details
+      # commit.message -- commit in repo on branch
+      str = "<b>#{pluralize(data.commits.try(:size), 'commit')} in <a href=#{data.repository.html_url}>#{data.repository.name}</a> on #{branch} </b></br>"
+      commits.each do |commit|
+        str << "#{commit.message} -- #{create_link(commit.url, 'commit')} in #{create_link(data.repository.html_url, data.repository.name)} on #{create_link(branch_url, branch)}</br>"
+      end
+      str
     end
   end
 end
