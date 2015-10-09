578661c76e1b852a23885542208220ed80d0fbc1
Faizal Zakaria <phaibusiness@gmail.com>
2015-10-09 18:28:15 +0800

    Add Mandrill in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :enable_starttls_auto => true,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"],
+    :authentication => 'login',
+    :domain => ENV["MAIL_DOMAIN"]
+  }
 end
