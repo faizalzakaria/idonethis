22344a592735d7c30e20fd234e78f408ec533075
Faizal Zakaria <phaibusiness@gmail.com>
2015-10-10 00:18:27 +0800

    Fixed host problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => ENV["MAIL_DOMAIN"] }
 
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
