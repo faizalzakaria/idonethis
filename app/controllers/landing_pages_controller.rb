4f4023e7cff838acd66e574aaa636f1478b8c470
Faizal Zakaria <phaibusiness@gmail.com>
2015-11-13 10:51:46 +0800

    Update views

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,5 +1,7 @@
 class LandingPagesController < ApplicationController
   respond_to :html
+  layout 'landing_page'
+
   def index
   end
 end
--- app/helpers/landing_pages_helper.rb
+++ app/helpers/landing_pages_helper.rb
@@ -1,2 +1,6 @@
 module LandingPagesHelper
+  def active?(pattern)
+    return 'active' unless params[:controller].match(pattern).nil?
+    ''
+  end
 end
